9523cf1fc989b5df2ad1bb13bb15f64680d0e3a5
glomph <glompho@gmail.com>
2024-12-18 23:03:58 +0000

    Finally did part one with a semi Dijkstra's algorithm. Was annoyed not to be finding the route but now not sure that matters for part 2

--- lib/advent_of_code/day_18.ex
+++ lib/advent_of_code/day_18.ex
@@ -5,8 +5,8 @@ defmodule AdventOfCode.Day18 do
     for y <- 0..ey do
       for x <- 0..ex do
         case grid[{x, y}] do
-          "#" -> "#"
-          _ -> "."
+          nil -> "."
+          char -> char
         end
       end
       |> IO.puts()
@@ -15,49 +15,87 @@ defmodule AdventOfCode.Day18 do
     grid
   end
 
-  def path_find(grid, {x, y}, {ex, ey}, route, visited) do
+  def not_legal_cell?(grid, {x, y}, {ex, ey}) do
+    x < 0 or x > ex or y < 0 or y > ey or grid[{x, y}] == "#"
+  end
+
+  def path_find(grid, {x, y}, {ex, ey}, score, visited) do
     directions = [{0, 1}, {1, 0}, {0, -1}, {-1, 0}]
     # IO.inspect({{x, y}, {ex, ey}})
     # IO.inspect({x, y, x < 0, x >= ex, y < 0, y >= ey, grid[{x, y}] == "#", visited[{x, y}]})
 
     cond do
-      x < 0 or x > ex or y < 0 or y > ey or grid[{x, y}] == "#" or visited[{x, y}] == true ->
-        []
+      not_legal_cell?(grid, {x, y}, {ex, ey}) or visited[{x, y}] == true ->
+        {:error, "not a legitimate cell"}
 
       x == ex and y == ey ->
-        route
+        {score, visited}
 
       true ->
         new_visited = Map.put(visited, {x, y}, true)
+        new_score = score + 1
 
-        paths =
-          Enum.map(directions, fn {dx, dy} ->
-            path_find(grid, {x + dx, y + dy}, {ex, ey}, [{x, y} | route], new_visited)
+        new_grid =
+          Enum.reduce(directions, grid, fn {dx, dy}, grid ->
+            {new_x, new_y} = {x + dx, y + dy}
+
+            cond do
+              not_legal_cell?(grid, {new_x, new_y}, {ex, ey}) ->
+                # IO.inspect("NOT LEGIT")
+                grid
+
+              Map.get(grid, {new_x, new_y}, :infinity) > new_score ->
+                # IO.inspect("UPDATE")
+                Map.put(grid, {new_x, new_y}, new_score)
+
+              true ->
+                # IO.inspect("FALLBACK")
+                grid
+            end
           end)
-          |> Enum.reject(&(&1 == []))
 
-        if paths == [] do
-          []
-        else
-          Enum.min_by(paths, &Enum.count/1)
-        end
+        # IO.inspect(new_visited, label: "visited")
+
+        {{nx, ny}, value} =
+          new_grid
+          |> Enum.reject(fn {key, _value} ->
+            Map.has_key?(new_visited, key)
+          end)
+          |> Enum.min_by(fn {_key, value} ->
+            case value do
+              "#" -> :infinity
+              _ -> value
+            end
+          end)
+
+        # |> IO.inspect()
+
+        path_find(new_grid, {nx, ny}, {ex, ey}, value, new_visited)
     end
   end
 
   def part1(input, ex \\ 70, ey \\ 70, bytes \\ 1024) do
-    input
-    |> String.split(["\n", ","])
-    |> Enum.map(&String.to_integer/1)
-    |> Enum.chunk_every(2)
-    |> Enum.take(bytes)
-    |> Enum.map(fn [x, y] -> {{x, y}, "#"} end)
-    |> Map.new()
+    map =
+      input
+      |> String.split(["\n", ","], trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.chunk_every(2)
+      |> Enum.take(bytes)
+      |> Enum.map(fn [x, y] -> {{x, y}, "#"} end)
+      |> Map.new()
+      |> print_grid(ex, ey)
+
+    {score, visited} = path_find(map, {0, 0}, {ex, ey}, 0, %{})
+
+    Enum.reduce(visited, map, fn {{x, y}, value}, map ->
+      Map.put(map, {x, y}, "X")
+    end)
     |> print_grid(ex, ey)
-    |> path_find({0, 0}, {ex, ey}, [], %{})
-    # |> List.flatten()
-    |> Enum.count()
+
+    score
   end
 
-  def part2(_args) do
+  def part2(input) do
+    part1(input, 6, 6, 13)
   end
 end
--- test/advent_of_code/day_18_test.exs
+++ test/advent_of_code/day_18_test.exs
@@ -34,11 +34,34 @@ defmodule AdventOfCode.Day18Test do
     assert result == 22
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
+    input = "5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0"
+    result = part1(input, 6, 6, 1)
 
-    assert result
+    assert result == "fail!"
   end
 end
