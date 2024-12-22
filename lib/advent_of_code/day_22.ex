17691435f48fac59cda765e444f946906b6b4267
glomph <glompho@gmail.com>
2024-12-22 12:27:26 +0000

    Part 22. Not too much trouble once I read everything more carefully.

--- lib/advent_of_code/day_22.ex
+++ lib/advent_of_code/day_22.ex
@@ -1,7 +1,85 @@
 defmodule AdventOfCode.Day22 do
-  def part1(_args) do
+  def mix(n, secret) do
+    Bitwise.bxor(n, secret)
   end
 
-  def part2(_args) do
+  def prune(n) do
+    rem(n, 16_777_216)
+  end
+
+  def mult(secret, n) do
+    (secret * n)
+    |> mix(secret)
+    |> prune()
+  end
+
+  def sdiv(secret, n) do
+    secret
+    |> div(n)
+    |> mix(secret)
+    |> prune()
+  end
+
+  def one_step(secret) do
+    secret
+    |> mult(64)
+    |> sdiv(32)
+    |> mult(2048)
+  end
+
+  def get_ones(n) do
+    rem(n, 10)
+  end
+
+  def differences(list) do
+    list
+    |> Enum.chunk(2, 1)
+    |> Enum.map(fn [a, b] -> {b, b - a} end)
+  end
+
+  def part1(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(fn n ->
+      Stream.iterate(n, &one_step/1)
+      |> Enum.at(2000)
+    end)
+    |> Enum.sum()
+  end
+
+  def part2(args) do
+    possible_sales =
+      args
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.map(fn n ->
+        Stream.iterate(n, fn n -> one_step(n) end)
+        |> Enum.slice(0, 2001)
+        |> Enum.map(&get_ones/1)
+        |> differences()
+        |> Enum.chunk_every(4, 1, :discard)
+        |> Enum.map(fn [{_na, a}, {_nb, b}, {_nc, c}, {nd, d}] ->
+          {{a, b, c, d}, nd}
+        end)
+        |> Enum.reverse()
+        |> Map.new()
+      end)
+
+    possible_sales
+    |> Enum.map(&Map.keys/1)
+    |> List.flatten()
+    |> Enum.uniq()
+    |> Enum.map(fn key ->
+      totals =
+        possible_sales
+        |> Enum.map(fn sale_map ->
+          Map.get(sale_map, key, 0)
+        end)
+
+      {key, totals, totals |> Enum.sum()}
+    end)
+    |> Enum.max_by(fn {_key, _totals, value} -> value end)
+    |> elem(2)
   end
 end
--- test/advent_of_code/day_22_test.exs
+++ test/advent_of_code/day_22_test.exs
@@ -3,19 +3,63 @@ defmodule AdventOfCode.Day22Test do
 
   import AdventOfCode.Day22
 
-  @tag :skip
+  test "mix test" do
+    result = mix(15, 42)
+
+    assert result == 37
+  end
+
+  test "prune test" do
+    input = 100_000_000
+    result = prune(input)
+    assert result == 16_113_920
+  end
+
+  test "iterate test" do
+    result =
+      Stream.iterate(123, &one_step/1)
+      |> Enum.take(11)
+
+    assert result == [
+             123,
+             15_887_950,
+             16_495_136,
+             527_345,
+             704_524,
+             1_553_684,
+             12_683_156,
+             11_100_544,
+             12_249_484,
+             7_753_432,
+             5_908_254
+           ]
+  end
+
   test "part1" do
-    input = nil
+    input = "1
+10
+100
+2024"
     result = part1(input)
 
-    assert result
+    assert result == 37_327_623
   end
 
   @tag :skip
-  test "part2" do
-    input = nil
+  test "part2 small" do
+    input = "123"
     result = part2(input)
 
-    assert result
+    assert result == 6
+  end
+
+  test "part2" do
+    input = "1
+2
+3
+2024"
+    result = part2(input)
+
+    assert result == 23
   end
 end
