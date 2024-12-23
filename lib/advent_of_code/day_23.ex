3efeb0443df3106947d5bd6f12399febda987d08
glomph <glompho@gmail.com>
2024-12-23 23:52:14 +0000

    Part 1 done. Went astray trying to get the answer with set interseciton. Maybe that is the way for part 2?

--- lib/advent_of_code/day_23.ex
+++ lib/advent_of_code/day_23.ex
@@ -1,5 +1,51 @@
+#
 defmodule AdventOfCode.Day23 do
-  def part1(_args) do
+  def find_cycles(start_node, end_node, size, route, network) do
+    cond do
+      size > 0 ->
+        Enum.reduce(network[start_node], [], fn next_node, acc ->
+          find_cycles(next_node, end_node, size - 1, [start_node | route], network) ++ acc
+        end)
+
+      size == 0 and start_node == end_node ->
+        [route |> Enum.reverse()]
+
+      size == 0 ->
+        []
+
+      true ->
+        nil
+    end
+    |> Enum.map(&MapSet.new/1)
+    |> Enum.uniq()
+  end
+
+  def part1(args) do
+    connections =
+      args
+      |> String.split(["\n", "-"], trim: true)
+      |> Enum.chunk_every(2)
+      |> Enum.reduce(%{}, fn [a, b], acc ->
+        acc
+        |> Map.update(a, MapSet.new([b]), fn current ->
+          MapSet.put(current, b)
+        end)
+        |> Map.update(b, MapSet.new([a]), fn current ->
+          MapSet.put(current, a)
+        end)
+      end)
+
+    connections
+    |> Map.keys()
+    |> Enum.filter(fn name ->
+      case name do
+        "t" <> _ -> true
+        _other -> false
+      end
+    end)
+    |> Enum.flat_map(&find_cycles(&1, &1, 3, [], connections))
+    |> Enum.uniq()
+    |> Enum.count()
   end
 
   def part2(_args) do
--- test/advent_of_code/day_23_test.exs
+++ test/advent_of_code/day_23_test.exs
@@ -3,12 +3,43 @@ defmodule AdventOfCode.Day23Test do
 
   import AdventOfCode.Day23
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = "kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn
+"
     result = part1(input)
 
-    assert result
+    assert result == 7
   end
 
   @tag :skip
