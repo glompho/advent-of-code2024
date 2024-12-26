ab6e13aaabbc4db714d770f6e66507ee556b04dd
glomph <glompho@gmail.com>
2024-12-26 12:43:47 +0000

    Very slight tidy

--- lib/advent_of_code/day_25.ex
+++ lib/advent_of_code/day_25.ex
@@ -1,8 +1,6 @@
 defmodule AdventOfCode.Day25 do
   def parse_one(input) do
-    lines =
-      input
-      |> String.split("\n")
+    lines = String.split(input, "\n")
 
     totals =
       Enum.reduce(lines, %{}, fn line, acc ->
@@ -30,8 +28,7 @@ defmodule AdventOfCode.Day25 do
       |> Enum.map(&parse_one/1)
       |> Enum.group_by(fn {type, _totals} -> type end, fn {_type, totals} -> totals end)
 
-    parsed[:locks]
-    |> Enum.reduce(0, fn lock, acc ->
+    Enum.reduce(parsed[:locks], 0, fn lock, acc ->
       Enum.reduce(parsed[:keys], acc, fn key, acc ->
         max_clash =
           Map.merge(key, lock, fn _k, v1, v2 ->
