99ad1ae81cff32b5c4a1770e00f1ee9393ecf5fa
glomph <glompho@gmail.com>
2024-12-28 16:08:56 +0000

    Spent far too long not understanding the question. Had to come back to it with fresh eyes.

--- lib/advent_of_code/day_23.ex
+++ lib/advent_of_code/day_23.ex
@@ -1,5 +1,9 @@
 #
 defmodule AdventOfCode.Day23 do
+  def find_cycles(start_node, size, network) do
+    find_cycles(start_node, start_node, size, [], network)
+  end
+
   def find_cycles(start_node, end_node, size, route, network) do
     cond do
       size > 0 ->
@@ -20,20 +24,23 @@ defmodule AdventOfCode.Day23 do
     |> Enum.uniq()
   end
 
-  def part1(args) do
-    connections =
-      args
-      |> String.split(["\n", "-"], trim: true)
-      |> Enum.chunk_every(2)
-      |> Enum.reduce(%{}, fn [a, b], acc ->
-        acc
-        |> Map.update(a, MapSet.new([b]), fn current ->
-          MapSet.put(current, b)
-        end)
-        |> Map.update(b, MapSet.new([a]), fn current ->
-          MapSet.put(current, a)
-        end)
+  def parse(args) do
+    args
+    |> String.split(["\n", "-", " "], trim: true)
+    |> Enum.chunk_every(2)
+    |> Enum.reduce(%{}, fn [a, b], acc ->
+      acc
+      |> Map.update(a, MapSet.new([b]), fn current ->
+        MapSet.put(current, b)
       end)
+      |> Map.update(b, MapSet.new([a]), fn current ->
+        MapSet.put(current, a)
+      end)
+    end)
+  end
+
+  def part1(args) do
+    connections = parse(args)
 
     connections
     |> Map.keys()
@@ -43,11 +50,35 @@ defmodule AdventOfCode.Day23 do
         _other -> false
       end
     end)
-    |> Enum.flat_map(&find_cycles(&1, &1, 3, [], connections))
+    |> Enum.flat_map(&find_cycles(&1, 3, connections))
     |> Enum.uniq()
     |> Enum.count()
   end
 
-  def part2(_args) do
+  def find_largest_clique(node, network) do
+    # finds the largest clique from a particular node in the network
+    Enum.reduce(network[node], MapSet.new([node]), fn next_node, acc ->
+      case MapSet.intersection(network[next_node], acc) do
+        # if the intersection is equal to the clique so far
+        # then it is connected to all current members
+        ^acc -> MapSet.put(acc, next_node)
+        _ -> acc
+      end
+    end)
+  end
+
+  def part2(args) do
+    connections = parse(args)
+
+    connections
+    |> Map.keys()
+    |> Enum.reduce(_cliques = [], fn node, cliques ->
+      largest_clique = find_largest_clique(node, connections)
+      [largest_clique | cliques]
+    end)
+    |> Enum.max()
+    |> MapSet.to_list()
+    |> Enum.sort()
+    |> Enum.join(",")
   end
 end
